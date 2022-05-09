be3e8a8742e7a07c4e9ec527dd8cfd7bd20ee7fe
ben-daniel2265 <ben.daniel@campus.technion.ac.il>
2022-05-09 19:22:37 +0300

    player.h

---
+++ Card.cpp
@@ -0,0 +1,31 @@
+#ifndef EX2_Card_SOURCE
+#define EX2_Card_SOURCE
+
+#include "Card.h"
+#include "utilities.h"
+#include "Player.h"
+
+
+Card::Card(CardType type, const CardStats& stats)
+{
+    
+}
+
+
+void Card::applyEncounter(Player& player) const
+{
+    
+}
+
+
+void Card::printInfo() const
+{
+
+}
+
+
+
+
+
+
+#endif //EX2_Card_SOURCE
